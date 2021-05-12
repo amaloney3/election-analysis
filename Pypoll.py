fe0b0dbe424905d43918ee8b1c122359d7bfcb19
amaloney3 <amaloney3@gmail.com>
2021-05-11 21:32:02 -0500

    Add files via upload

--- Pypoll.py
+++ Pypoll.py
@@ -13,6 +13,12 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#initialize vote accumulator to zero
+total_votes=0
+
+#Declare list of candidate options
+candidate_options = []
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -20,3 +26,23 @@ with open(file_to_load) as election_data:
     # Read and print the header row.
     headers = next(file_reader)
     print(headers)
+
+    #Print each row in the CSV file
+    for row in file_reader:
+
+        #Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+#Print candidate list
+print(candidate_options)
+
+# 3. Print total votes
+print(total_votes)
